cf32d3194a44cc42d2a997da084111e93beccdca
Nikhil Nar <ncn251@nyu.edu>
2019-03-13 20:58:26 -0400

    Changed map reduce code

--- src/mapper.py
+++ src/mapper.py
@@ -4,29 +4,30 @@
 import sys
 import re
 import os
+import io
 
 
-# def read_input(file):
-#     print("file=", file)
-#     for line in file:
-#         # split the line into words
-#         line = re.sub('([^\s\w]|_)+', '', line).lower()
-#         yield line.split()
+def read_input(file):
+    for line in file:
+        # split the line into words
+        line = re.sub('([^\s\w]|_)+', '', line).lower()
+        yield line.split()
 
-def read_input(path):
 
-    for filename in os.listdir(path):
-        with open(path+filename, "r") as f:
-            for line in f.readlines():
-                # split the line into words
-                line = re.sub('([^\s\w]|_)+', ' ', line).lower()
-                yield line.split()
+# def read_input(path):
+#     print("path=", path)
+#     for filename in os.listdir(path):
+#         with open(path+filename, "r") as f:
+#             for line in f.readlines():
+#                 # split the line into words
+#                 line = re.sub('([^\s\w]|_)+', ' ', line).lower()
+#                 yield line.split()
 
 
 def main(separator='\t'):
     # input comes from STDIN (standard input)
-    #data = read_input(sys.stdin)
-    data = read_input(sys.argv[1])
+    data = read_input(sys.stdin)
+    #data = read_input(sys.argv[1])
     for words in data:
         # write the results to STDOUT (standard output);
         # what we output here will be the input for the
